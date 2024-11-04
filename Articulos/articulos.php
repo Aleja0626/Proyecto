1b8513d270ed430491f71803094be108863474cf
Aleja0626 <alejandra.mendoza26@hotmail.com>
2024-11-04 22:58:52 +0100

    hola

---
+++ Articulos/articulos.php
@@ -0,0 +1,336 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>articulos</title>
+	<link rel="stylesheet" href="../CSS/articulos.css">
+	<link rel="stylesheet" href="../CSS/style.css">
+</head>
+<body>
+    <header>
+        <?php include '/includes/header.php';?>    
+    </header>
+    <section id ="artPrincipal">
+        <div id="pPrincipal">
+            <h1>ARTICULOS</h1>
+            <img src="./MEDIA/" alt="">
+        </div>
+    </section>
+    <!-- SECCIONES DE ARTICULOS -->
+    <section class="articulos">
+	 <!-- ARTICULO 1 -->			
+				<h2>Articulo 1</h2>
+			
+			<div class="contenedorTendencia">
+
+				<div class="columTendencia">
+					<img src="Media/tendencia1.jpeg" id="imgTendencia" alt="">
+				</div>
+
+				<div class="textoTendencia">
+					<p>
+						"Somos una tienda de ropa exclusiva para niñas brindando a sus clientas altos estándares de calidad.
+						Especializados en vestidos para cumpleaños y ocasiones especiales.
+						<br></br>
+						Estamos a la vanguardia de la moda infantil, tendencias.
+						Fomentamos valores positivos y una cultura de autoestima saludable entre las niñas, enseñándoles a expresarse a través de la moda y la creatividad, y contribuyendo así a su desarrollo integral.
+						<br></br>		
+						Nos aseguramos de la durabilidad de las prendas y estamos comprometidos con la sostenibilidad en los procesos de producción y distribución, contribuyendo así al cuidado del medio ambiente "
+
+					</p>   
+				</div>
+                <a id ="verArticulo" href="Articulos/articulo1.html" class="button">Ver Artículo</a>
+			</div>
+		</section>  
+        <!-- ARTICULO 2 -->
+        <section class="articulos">
+				
+				<h2>articulo 2</h2>
+			
+			<div class="contenedorTendencia">
+
+				<div class="columTendencia">
+					<img src="Media/tendencia1.jpeg" id="imgTendencia" alt="">
+				</div>
+
+				<div class="textoTendencia">
+					<p>
+						"Somos una tienda de ropa exclusiva para niñas brindando a sus clientas altos estándares de calidad.
+						Especializados en vestidos para cumpleaños y ocasiones especiales.
+						<br></br>
+						Estamos a la vanguardia de la moda infantil, tendencias.
+						Fomentamos valores positivos y una cultura de autoestima saludable entre las niñas, enseñándoles a expresarse a través de la moda y la creatividad, y contribuyendo así a su desarrollo integral.
+						<br></br>		
+						Nos aseguramos de la durabilidad de las prendas y estamos comprometidos con la sostenibilidad en los procesos de producción y distribución, contribuyendo así al cuidado del medio ambiente "
+
+					</p>   
+				</div>
+                <a id ="verArticulo" href="Articulos/articulo1.html" class="button">Ver Artículo</a>
+			</div>
+		</section>  
+         <!-- ARTICULO 3 -->
+         <section class="articulos">
+				
+				<h2>articulo 3</h2>
+			
+			<div class="contenedorTendencia">
+
+				<div class="columTendencia">
+					<img src="Media/tendencia1.jpeg" id="imgTendencia" alt="">
+				</div>
+
+				<div class="textoTendencia">
+					<p>
+						"Somos una tienda de ropa exclusiva para niñas brindando a sus clientas altos estándares de calidad.
+						Especializados en vestidos para cumpleaños y ocasiones especiales.
+						<br></br>
+						Estamos a la vanguardia de la moda infantil, tendencias.
+						Fomentamos valores positivos y una cultura de autoestima saludable entre las niñas, enseñándoles a expresarse a través de la moda y la creatividad, y contribuyendo así a su desarrollo integral.
+						<br></br>		
+						Nos aseguramos de la durabilidad de las prendas y estamos comprometidos con la sostenibilidad en los procesos de producción y distribución, contribuyendo así al cuidado del medio ambiente "
+
+					</p>   
+				</div>
+                <a id ="verArticulo" href="Articulos/articulo3.html" class="button">Ver Artículo</a>
+			</div>
+		</section> 
+         <!-- ARTICULO 4 -->
+         <section class="articulos">
+				
+				<h2>articulo 4</h2>
+			
+			<div class="contenedorTendencia">
+
+				<div class="columTendencia">
+					<img src="Media/tendencia1.jpeg" id="imgTendencia" alt="">
+				</div>
+
+				<div class="textoTendencia">
+					<p>
+						"Somos una tienda de ropa exclusiva para niñas brindando a sus clientas altos estándares de calidad.
+						Especializados en vestidos para cumpleaños y ocasiones especiales.
+						<br></br>
+						Estamos a la vanguardia de la moda infantil, tendencias.
+						Fomentamos valores positivos y una cultura de autoestima saludable entre las niñas, enseñándoles a expresarse a través de la moda y la creatividad, y contribuyendo así a su desarrollo integral.
+						<br></br>		
+						Nos aseguramos de la durabilidad de las prendas y estamos comprometidos con la sostenibilidad en los procesos de producción y distribución, contribuyendo así al cuidado del medio ambiente "
+
+					</p>   
+				</div>
+                <a id ="verArticulo" href="Articulos/articulo4.html" class="button">Ver Artículo</a>
+			</div>
+		</section> 
+         <!-- ARTICULO 5 -->
+         <section class="articulos">
+				
+				<h2>articulo 5</h2>
+			
+			<div class="contenedorTendencia">
+
+				<div class="columTendencia">
+					<img src="Media/tendencia1.jpeg" id="imgTendencia" alt="">
+				</div>
+
+				<div class="textoTendencia">
+					<p>
+						"Somos una tienda de ropa exclusiva para niñas brindando a sus clientas altos estándares de calidad.
+						Especializados en vestidos para cumpleaños y ocasiones especiales.
+						<br></br>
+						Estamos a la vanguardia de la moda infantil, tendencias.
+						Fomentamos valores positivos y una cultura de autoestima saludable entre las niñas, enseñándoles a expresarse a través de la moda y la creatividad, y contribuyendo así a su desarrollo integral.
+						<br></br>		
+						Nos aseguramos de la durabilidad de las prendas y estamos comprometidos con la sostenibilidad en los procesos de producción y distribución, contribuyendo así al cuidado del medio ambiente "
+
+					</p>   
+				</div>
+                <a id ="verArticulo" href="Articulos/articulo5.html" class="button">Ver Artículo</a>
+			</div>
+		</section> 
+         <!-- ARTICULO 6 -->
+         <section class="articulos">
+				
+				<h2>articulo 6</h2>
+			
+			<div class="contenedorTendencia">
+
+				<div class="columTendencia">
+					<img src="Media/tendencia1.jpeg" id="imgTendencia" alt="">
+				</div>
+
+				<div class="textoTendencia">
+					<p>
+						"Somos una tienda de ropa exclusiva para niñas brindando a sus clientas altos estándares de calidad.
+						Especializados en vestidos para cumpleaños y ocasiones especiales.
+						<br></br>
+						Estamos a la vanguardia de la moda infantil, tendencias.
+						Fomentamos valores positivos y una cultura de autoestima saludable entre las niñas, enseñándoles a expresarse a través de la moda y la creatividad, y contribuyendo así a su desarrollo integral.
+						<br></br>		
+						Nos aseguramos de la durabilidad de las prendas y estamos comprometidos con la sostenibilidad en los procesos de producción y distribución, contribuyendo así al cuidado del medio ambiente "
+
+					</p>   
+				</div>
+                <a id ="verArticulo" href="Articulos/articulo6.html" class="button">Ver Artículo</a>
+			</div>
+		</section> 
+         <!-- ARTICULO 7 -->
+         <section class="articulos">
+				
+				<h2>articulo 7</h2>
+			
+			<div class="contenedorTendencia">
+
+				<div class="columTendencia">
+					<img src="Media/tendencia1.jpeg" id="imgTendencia" alt="">
+				</div>
+
+				<div class="textoTendencia">
+					<p>
+						"Somos una tienda de ropa exclusiva para niñas brindando a sus clientas altos estándares de calidad.
+						Especializados en vestidos para cumpleaños y ocasiones especiales.
+						<br></br>
+						Estamos a la vanguardia de la moda infantil, tendencias.
+						Fomentamos valores positivos y una cultura de autoestima saludable entre las niñas, enseñándoles a expresarse a través de la moda y la creatividad, y contribuyendo así a su desarrollo integral.
+						<br></br>		
+						Nos aseguramos de la durabilidad de las prendas y estamos comprometidos con la sostenibilidad en los procesos de producción y distribución, contribuyendo así al cuidado del medio ambiente "
+
+					</p>   
+				</div>
+                <a id ="verArticulo" href="Articulos/articulo7.html" class="button">Ver Artículo</a>
+			</div>
+		</section> 
+         <!-- ARTICULO 8 -->
+         <section class="articulos">
+				
+				<h2>articulo 8</h2>
+			
+			<div class="contenedorTendencia">
+
+				<div class="columTendencia">
+					<img src="Media/tendencia1.jpeg" id="imgTendencia" alt="">
+				</div>
+
+				<div class="textoTendencia">
+					<p>
+						"Somos una tienda de ropa exclusiva para niñas brindando a sus clientas altos estándares de calidad.
+						Especializados en vestidos para cumpleaños y ocasiones especiales.
+						<br></br>
+						Estamos a la vanguardia de la moda infantil, tendencias.
+						Fomentamos valores positivos y una cultura de autoestima saludable entre las niñas, enseñándoles a expresarse a través de la moda y la creatividad, y contribuyendo así a su desarrollo integral.
+						<br></br>		
+						Nos aseguramos de la durabilidad de las prendas y estamos comprometidos con la sostenibilidad en los procesos de producción y distribución, contribuyendo así al cuidado del medio ambiente "
+
+					</p>   
+				</div>
+                <a id ="verArticulo" href="Articulos/articulo8.html" class="button">Ver Artículo</a>
+			</div>
+		</section> 
+         <!-- ARTICULO 9 -->
+         <section class="articulos">
+				
+				<h2>articulo 9</h2>
+			
+			<div class="contenedorTendencia">
+
+				<div class="columTendencia">
+					<img src="Media/tendencia1.jpeg" id="imgTendencia" alt="">
+				</div>
+
+				<div class="textoTendencia">
+					<p>
+						"Somos una tienda de ropa exclusiva para niñas brindando a sus clientas altos estándares de calidad.
+						Especializados en vestidos para cumpleaños y ocasiones especiales.
+						<br></br>
+						Estamos a la vanguardia de la moda infantil, tendencias.
+						Fomentamos valores positivos y una cultura de autoestima saludable entre las niñas, enseñándoles a expresarse a través de la moda y la creatividad, y contribuyendo así a su desarrollo integral.
+						<br></br>		
+						Nos aseguramos de la durabilidad de las prendas y estamos comprometidos con la sostenibilidad en los procesos de producción y distribución, contribuyendo así al cuidado del medio ambiente "
+
+					</p>   
+				</div>
+                <a id ="verArticulo" href="Articulos/articulo9.html" class="button">Ver Artículo</a>
+			</div>
+		</section> 
+         <!-- ARTICULO 10 -->
+         <section class="articulos">
+				
+				<h2>articulo 10</h2>
+			
+			<div class="contenedorTendencia">
+
+				<div class="columTendencia">
+					<img src="Media/tendencia1.jpeg" id="imgTendencia" alt="">
+				</div>
+
+				<div class="textoTendencia">
+					<p>
+						"Somos una tienda de ropa exclusiva para niñas brindando a sus clientas altos estándares de calidad.
+						Especializados en vestidos para cumpleaños y ocasiones especiales.
+						<br></br>
+						Estamos a la vanguardia de la moda infantil, tendencias.
+						Fomentamos valores positivos y una cultura de autoestima saludable entre las niñas, enseñándoles a expresarse a través de la moda y la creatividad, y contribuyendo así a su desarrollo integral.
+						<br></br>		
+						Nos aseguramos de la durabilidad de las prendas y estamos comprometidos con la sostenibilidad en los procesos de producción y distribución, contribuyendo así al cuidado del medio ambiente "
+
+					</p>   
+				</div>
+                <a id ="verArticulo" href="Articulos/articulo10.html" class="button">Ver Artículo</a>
+			</div>
+		</section> 
+         <!-- ARTICULO 11 -->
+         <section class="articulos">
+				
+				<h2>articulo 11</h2>
+			
+			<div class="contenedorTendencia">
+
+				<div class="columTendencia">
+					<img src="Media/tendencia1.jpeg" id="imgTendencia" alt="">
+				</div>
+
+				<div class="textoTendencia">
+					<p>
+						"Somos una tienda de ropa exclusiva para niñas brindando a sus clientas altos estándares de calidad.
+						Especializados en vestidos para cumpleaños y ocasiones especiales.
+						<br></br>
+						Estamos a la vanguardia de la moda infantil, tendencias.
+						Fomentamos valores positivos y una cultura de autoestima saludable entre las niñas, enseñándoles a expresarse a través de la moda y la creatividad, y contribuyendo así a su desarrollo integral.
+						<br></br>		
+						Nos aseguramos de la durabilidad de las prendas y estamos comprometidos con la sostenibilidad en los procesos de producción y distribución, contribuyendo así al cuidado del medio ambiente "
+
+					</p>   
+				</div>
+                <a id ="verArticulo" href="Articulos/articulo11.html" class="button">Ver Artículo</a>
+			</div>
+		</section> 
+         <!-- ARTICULO 12 -->
+         <section class="articulos">
+				
+				<h2>articulo 12</h2>
+			
+			<div class="contenedorTendencia">
+
+				<div class="columTendencia">
+					<img src="Media/tendencia1.jpeg" id="imgTendencia" alt="">
+				</div>
+
+				<div class="textoTendencia">
+					<p>
+						"Somos una tienda de ropa exclusiva para niñas brindando a sus clientas altos estándares de calidad.
+						Especializados en vestidos para cumpleaños y ocasiones especiales.
+						<br></br>
+						Estamos a la vanguardia de la moda infantil, tendencias.
+						Fomentamos valores positivos y una cultura de autoestima saludable entre las niñas, enseñándoles a expresarse a través de la moda y la creatividad, y contribuyendo así a su desarrollo integral.
+						<br></br>		
+						Nos aseguramos de la durabilidad de las prendas y estamos comprometidos con la sostenibilidad en los procesos de producción y distribución, contribuyendo así al cuidado del medio ambiente "
+
+					</p>   
+				</div>
+                <a id ="verArticulo" href="Articulos/articulo12.html" class="button">Ver Artículo</a>
+			</div>
+		</section> 
+    <footer>
+    <?php //include './includes/footer.php'; ?>
+    </footer>
+</body>
+</html>
---
+++ JS/script.js
@@ -0,0 +1,121 @@
+// REALIZACION DEL LLAMADO A HACER EL SLIDESHOW DEL HEROIMAGE 
+
+let bgcont =0;
+function heroSlideShow(){
+
+	let listImgHero = [
+
+		"url('MEDIA/Hero0.png')",
+		"url('MEDIA/Hero1.jpeg')",
+		"url('MEDIA/Hero2.jpg')",
+		"url('MEDIA/Hero3.jpeg')",
+		"url('MEDIA/Hero4.jpeg')"
+
+		];
+
+	bgcont++;
+
+	if (bgcont == listImgHero.length) {
+
+		bgcont =0;
+	}
+
+	document.getElementById("heroImage").style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3))," +  listImgHero [bgcont];
+}
+
+
+// CODIGO SLIDESHOW  CON ANIMACION DEL HERO IMAGE
+
+let contAnim =0;
+let contMain =0;
+
+function imagenSalienteAnim() {
+
+	let listImgHeroAnim = document.getElementsByClassName("fondoHero");
+
+	contAnim++;
+	contMain = contAnim-1;
+
+	if (contAnim == listImgHeroAnim.length){
+
+		contAnim = 0;
+	}
+
+	if (contMain < 0){
+
+		contMain = listImgHeroAnim.length-1;
+	}
+
+	for (let i=0; i<listImgHeroAnim.length; i++) {
+
+		listImgHeroAnim[i].classList.remove("imagenEntrante");
+		listImgHeroAnim[i].classList.remove("imagenSaliente");
+		listImgHeroAnim[i].classList.remove("imagenSuelta");
+
+		if(i==contAnim){
+
+			listImgHeroAnim[i].classList.add("imagenEntrante");
+		}
+
+		else if(i== contMain){
+			listImgHeroAnim[i].classList.add("imagenSaliente");
+		}
+
+		else {
+			listImgHeroAnim[i].classList.add("imagenSuelta");
+		}
+	}	
+
+}
+
+
+// MODAL PARA ENVIAR LA CONFIRMACION DEL FORMULARIO
+
+function modalContacto() {
+	document.getElementById("modalContacto").style.display="block";
+	document.documentElement.style.overflow="hidden";
+
+	let nombre = document.getElementById("formNombre").value;
+	let email = document.getElementById("formCorreo").value;
+
+	let mensaje;
+
+	(function formCheck(){
+
+		if (!document.getElementById("formCorreo").checkValidity() && !document.getElementById("formNombre").checkValidity()){
+			mensaje = "Aún no haz ingresado tus datos.";
+			document.getElementById("mensajeC").innerHTML = mensaje;
+		}
+
+		else if(!document.getElementById("formNombre").checkValidity()){
+			mensaje = "No haz ingresado tu Nombre correctamente";
+			document.getElementById("mensajeC").innerHTML = mensaje;
+		}
+
+		else if (!document.getElementById("formCorreo").checkValidity()){
+			mensaje = "Datos Incorrectos<br>Verifica que el correo electrónico este escrito correctamente";
+			document.getElementById("mensajeC").innerHTML = mensaje;
+		}
+		
+		else {
+
+			mensaje = nombre + "<br>"+" Muchas gracias por suscribirte!";
+
+			document.getElementById("mensajeC").innerHTML = mensaje;
+
+			//LINEA PARA PODER CERRAR EL MODAL CONTACTO CON EL TABULADOR
+			document.getElementById("bottonCerrarMContacto").focus();
+		}	
+
+	})();
+}
+
+// FUNCION QUE NOS PERMITE CERRAR EL MODAL DEL FORMULARIO
+	function cerrarModalC(){
+		document.getElementById("modalContacto").style.display="none";
+		document.documentElement.style.overflow="auto";
+
+		document.getElementById("formNombre").value="";
+		document.getElementById("formCorreo").value="";
+
+	}
